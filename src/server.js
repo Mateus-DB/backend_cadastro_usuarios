93bd13b44978608416473af4d7e39b8305c468c7
mateus <demartinomateus@gmail.com>
2026-05-16 21:11:35 -0300

    modificando cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://frontend-cadastro-usuarios.vercel.app/"
+        "https://frontend-cadastro-usuarios.vercel.app"
     ]
 }));
 
